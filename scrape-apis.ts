6a7c2f8fd38a1b5a3bf692d5934a4905b1c39520
Wijnand Karsens <wijnand@karsens.com>
2024-04-21 20:01:36 +0200

    improvements

--- scrape-apis.ts
+++ scrape-apis.ts
@@ -3,11 +3,51 @@ import fs from "node:fs";
 import {
   OpenapiDocument,
   fetchOpenapi,
+  humanCase,
   mapMany,
   notEmpty,
   oneByOne,
+  slugify,
 } from "from-anywhere";
 import { writeJsonToFile } from "from-anywhere/node";
+
+const hardcodedItemObject = {
+  flyio: "https://docs.machines.dev/spec/openapi3.json",
+  heygen: "https://openai-plugin.heygen.com/openapi.yaml",
+  serperdev: "https://serper-dev.actionschema.com/openapi.json",
+  replicate: "https://api.replicate.com/openapi.json",
+  vapi: "https://api.vapi.ai/api-json",
+  klippa: "https://dochorizon.klippa.com/api/open-api.yaml",
+  // deepgram: ?????
+  uberduck: "https://api.uberduck.ai/openapi.json",
+  openai:
+    "https://raw.githubusercontent.com/openai/openai-openapi/master/openapi.yaml",
+  huggingface: "https://api.endpoints.huggingface.cloud/api-doc/openapi.json",
+  cloudflare:
+    "https://raw.githubusercontent.com/cloudflare/api-schemas/main/openapi.json",
+  twilio: "https://github.com/twilio/twilio-oai/tree/main/spec/json",
+  sendgrid:
+    "https://raw.githubusercontent.com/sendgrid/sendgrid-oai/main/oai.json",
+  linode: "https://www.linode.com/docs/api/openapi.yaml",
+  vercel: "https://openapi.vercel.sh/",
+  //(unofficial)
+  playht:
+    "https://raw.githubusercontent.com/cielo24/playht-openapi/main/playht.yml",
+};
+
+const hardcodedList = Object.keys(hardcodedItemObject).map(
+  (key) =>
+    ({
+      key,
+      originalUrl: hardcodedItemObject[key as keyof typeof hardcodedItemObject],
+      title: humanCase(key),
+    } satisfies OpenapiListItem),
+);
+export type OpenapiListItem = {
+  key: string;
+  originalUrl: string;
+  title: string | undefined;
+};
 /*
 - Make a script that:
   - lists all current JSONs in `public`
@@ -30,19 +70,19 @@ const scrapeApis = async () => {
     return;
   }
 
-  const list = await fetch("https://api.apis.guru/v2/list.json").then(
+  const apiObject = await fetch("https://api.apis.guru/v2/list.json").then(
     (res) => res.json() as Promise<ApiObject>,
   );
 
-  const parsedList = Object.keys(list)
+  const parsedList = Object.keys(apiObject)
     .map((key) => {
-      const latestVersion = Object.keys(list[key].versions).sort().pop();
+      const latestVersion = Object.keys(apiObject[key].versions).sort().pop();
 
       if (!latestVersion) {
         return;
       }
 
-      const version = list[key].versions[latestVersion];
+      const version = apiObject[key].versions[latestVersion];
 
       const originalUrl = version.info["x-origin"]
         // Only index openapis for now
@@ -54,29 +94,31 @@ const scrapeApis = async () => {
       }
 
       // Key must be good for subdomain
-      const realKey = key
-        .replaceAll("(", "-")
-        .replaceAll(")", "-")
-        .replaceAll(":", "-")
-        .replaceAll(".", "-")
-        .toLowerCase();
+      const realKey = slugify(key).toLowerCase();
 
       return {
         key: realKey,
         originalUrl,
         title: version.info.title,
         // description: version.info.description,
-      };
+      } satisfies OpenapiListItem;
     })
     .filter(notEmpty);
 
-  console.log("Writing list", parsedList.length);
+  const fullList = parsedList.concat(hardcodedList);
+
+  console.log(
+    "Writing list",
+    parsedList.length,
+    hardcodedList.length,
+    fullList.length,
+  );
 
   const downloadedKeys = (
     await mapMany(
-      parsedList,
+      fullList,
       async (item, index) => {
-        console.log(`${index + 1}/${parsedList.length}`);
+        console.log(`${index + 1}/${fullList.length}`);
         const downloaded = await fetchOpenapi(item.originalUrl);
 
         if (!downloaded || typeof downloaded === "string") {
