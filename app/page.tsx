39df7821b793626fcf0c751f63922fbcb14f5f50
Wijnand Karsens <wijnand@karsens.com>
2024-04-24 08:57:25 +0200

    improvements

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+const HomePage = () => {
+  return (
+    <div className="h-full p-4">
+      <h1 className="text-3xl">OpenAPI Enhancement Proxy</h1>
+      <p>
+        See instructions in our{" "}
+        <a href="https://github.com/CodeFromAnywhere/openapi-enhancement-proxy-next">
+          GitHub Repo
+        </a>
+      </p>
+    </div>
+  );
+};
+
+export default HomePage;
