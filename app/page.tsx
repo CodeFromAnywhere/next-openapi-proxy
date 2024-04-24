1d84396371aa94a77ba30a228059c32fcf7b323c
Wijnand Karsens <wijnand@karsens.com>
2024-04-24 08:59:07 +0200

    improvements

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,19 @@ const HomePage = () => {
       <h1 className="text-3xl">OpenAPI Enhancement Proxy</h1>
       <p>
         See instructions in our{" "}
-        <a href="https://github.com/CodeFromAnywhere/openapi-enhancement-proxy-next">
+        <a
+          className="text-blue-500"
+          href="https://github.com/CodeFromAnywhere/openapi-enhancement-proxy-next"
+        >
           GitHub Repo
         </a>
       </p>
 
       <p>
         Explore all our proxies in our{" "}
-        <a href="https://explorer.actionschema.com">OpenAPI Explorer</a>
+        <a className="text-blue-500" href="https://explorer.actionschema.com">
+          OpenAPI Explorer
+        </a>
       </p>
     </div>
   );
